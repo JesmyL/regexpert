b822858253099095650e7e2d2c84fba266021f07
PSlobodskoy <PSlobodskoy@one.kz>
2025-06-22 14:02:24 +0500

    fix(): simplify escapeRegExpSymbols()

--- src/escapeRegExpSymbols.ts
+++ src/escapeRegExpSymbols.ts
@@ -1,7 +1,2 @@
-const escapes: Record<string, string> = {};
 const symbolsReg = /([{}[\]?./\\*$^()+|])/g;
-
-export const escapeRegExpSymbols = (str: string) => {
-  if (escapes[str] !== undefined) return escapes[str];
-  return (escapes[str] = str.replace(symbolsReg, '\\$1'));
-};
+export const escapeRegExpSymbols = (str: string) => str.replace(symbolsReg, '\\$1');
